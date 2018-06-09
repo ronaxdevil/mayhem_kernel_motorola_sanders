209dcf785d5526344d4cb56fc2563282a82590a7
Wang Han <416810799@qq.com>
2018-06-08 22:36:57 -0700

    fs: sync: Avoid calling fdget without fdput
    When adding fsync support, we check for fsync_enabled() in several
    cases but it appears that we should fdput() after fdget() but the
    current code just check for fsync_enabled and directly return in
    some cases after calling fdget().
    
    Fix it by checking fsync_enabled first and move the f initialization
    after the check. Also remove some unnecessary fsync_enabled checks as
    this will be checked later in do_fsync().
    
    Signed-off-by: Wang Han <416810799@qq.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- fs/sync.c
+++ fs/sync.c
@@ -152,7 +152,7 @@ void emergency_sync(void)
  */
 SYSCALL_DEFINE1(syncfs, int, fd)
 {
-	struct fd f = fdget(fd);
+	struct fd f;
 	struct super_block *sb;
 	int ret;
 
@@ -220,12 +220,13 @@ EXPORT_SYMBOL(vfs_fsync);
 
 static int do_fsync(unsigned int fd, int datasync)
 {
-	struct fd f = fdget(fd);
+	struct fd f;
 	int ret = -EBADF;
 	
 	if (!fsync_enabled)
 		return 0;
 
+	f = fdget(fd);
 	if (f.file) {
 		ret = vfs_fsync(f.file, datasync);
 		fdput(f);
