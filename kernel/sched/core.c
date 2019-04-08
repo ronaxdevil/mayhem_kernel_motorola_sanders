d918552fd104ebe5b79a877021e718c81f1bf595
ronaxdevil <pratabidya.007@gmail.com>
2019-04-08 20:00:46 +0000

    sched: core.c: don't use 'fixup_busy_time' [-Werror,-Wimplicit-function-declaration]
    ../kernel/sched/core.c:1341:3: error: implicit declaration of function 'fixup_busy_time' [-Werror,-Wimplicit-function-declaration]
                    fixup_busy_time(p, new_cpu);
                    ^
    ../kernel/sched/core.c:1341:3: note: did you mean 'walt_fixup_busy_time'?
    ../kernel/sched/walt.h:27:6: note: 'walt_fixup_busy_time' declared here
    void walt_fixup_busy_time(struct task_struct *p, int new_cpu);
         ^
    ../kernel/sched/core.c:1341:3: error: this function declaration is not a prototype [-Werror,-Wstrict-prototypes]
                    fixup_busy_time(p, new_cpu);
                    ^
    
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1338,7 +1338,7 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 			p->sched_class->migrate_task_rq(p);
 		p->se.nr_migrations++;
 		perf_sw_event_sched(PERF_COUNT_SW_CPU_MIGRATIONS, 1, 0);
-		fixup_busy_time(p, new_cpu);
+		walt_fixup_busy_time(p, new_cpu);
 	}
 
 	__set_task_cpu(p, new_cpu);
